be53d63c3fe0564e9f0e4864a40609b2decd35f3
TyreseJORDANEBLAIR <>
2026-07-21 00:47:35 +0000

    Chained Conditions

---
+++ advanced_data_filtering/chained_conditions/query.sql
@@ -0,0 +1,3 @@
+SELECT name-- Write a SQL query to select only the names of countries from Asia with a population greater than 1000000
+FROM country
+WHERE continent = 'Asia' and population > 1000000;
